eb80a161538687865c6bc14702fba289b8bc21bc
Osiel Burga <osielburga@gmail.com>
2026-05-11 21:48:15 -0500

    Actualizaci├│n autom├ítica lun.-11-05 09-48

--- commit_push.bat
+++ commit_push.bat
@@ -1,8 +1,14 @@
 @echo off
 cd /d D:\VPNPortable
 
+:: Obtener fecha y hora actuales
+for /f "tokens=1-4 delims=/ " %%a in ('date /t') do set FECHA=%%a-%%b-%%c
+for /f "tokens=1-2 delims=: " %%a in ('time /t') do set HORA=%%a-%%b
+
+:: Crear commit con mensaje dinámico
 git add .
-git commit -m "Actualización automática"
+git commit -m "Actualización automática %FECHA% %HORA%"
 git push origin main
 
 pause
+
